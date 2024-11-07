f466769459e90c967b64ca03a8b229df96d65d8c
Diego <diego.cordoba.a@gmail.com>
2024-11-07 11:56:26 +0100

    Ejemplo de modificación de skeleton

---
+++ 7-Skeleton/cambio tema/js/funciones.js
@@ -0,0 +1,15 @@
+document.querySelector("body button:nth-child(1)").addEventListener("click",function(){
+    document.querySelector("body").classList.remove("claro");
+    document.querySelector("body").classList.add("oscuro");
+});
+
+document.querySelector("body button:nth-child(2)").addEventListener("click",function(){
+    document.querySelector("body").classList.remove("oscuro");
+    document.querySelector("body").classList.add("claro");
+});
+
+
+/* Para cambiar el tamaño de la barra de desplazamiento */
+document.querySelector(".barraDeProgreso").addEventListener("click",function(){
+    document.querySelector(".barra").style.width = "30%";
+});
