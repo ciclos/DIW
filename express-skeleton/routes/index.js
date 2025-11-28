90b0440ae88cbce9d70203732ca8e136af75b8de
Diego <diego.cordoba.a@gmail.com>
2025-11-28 12:08:38 +0100

    añadido @use a tam_lg y generación de colores

--- express-skeleton/routes/index.js
+++ express-skeleton/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('layout', { title: 'Express' });
+});
+
+router.get('/alternativo', function(req, res, next) {
+  res.render('layout_alternativo', { title: 'Express' });
 });
 
 module.exports = router;
